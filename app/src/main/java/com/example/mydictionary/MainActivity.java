ecb3c60396347d1e3405cfce3a05e3e1ec7fafb2
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-07-11 14:04:55 +0600

    mysingleton class

--- app/src/main/java/com/example/mydictionary/MainActivity.java
+++ app/src/main/java/com/example/mydictionary/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.mydictionary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText searchTv;
+    TextView word,definitionTv,exampleTv,originTv;
+    ImageView searchBtn,micBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchTv=findViewById(R.id.searchTv);
+        word=findViewById(R.id.word_id);
+        definitionTv=findViewById(R.id.definitionTv);
+        exampleTv=findViewById(R.id.exampleTv);
+        originTv=findViewById(R.id.originTv);
+        searchBtn=findViewById(R.id.SearchBtn);
+        micBtn=findViewById(R.id.speakBtn);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    private void getMeaning(String main_word){
+
+
     }
 }
